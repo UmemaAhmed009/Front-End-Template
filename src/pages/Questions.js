9f7890d0775c5bc3bf3cbd7917c1e20f0c2c840d
minalsarwar <minalsarwar51@gmail.com>
2023-05-27 01:28:25 +0500

    Final commit

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -9,38 +9,16 @@ import { useNavigate, Link, useParams } from 'react-router-dom';
 // components
 import Iconify from '../components/iconify';
 
-const buttonStyles = {
-  display: 'block',
-  margin: '10px',
-  padding: '8px 16px',
-  fontSize: '14px',
-  fontWeight: 'bold',
-  color: '#fff',
-  backgroundColor: '#D877E3',
-  border: 'none',
-  borderRadius: '4px',
-  cursor: 'pointer',
-  transition: 'background-color 0.3s',
-};
-
-const nextButtonStyles = {
-  display: 'block',
-  marginTop: '10px',
-  marginLeft: '100px',
-  padding: '10px 20px',
-  fontSize: '16px',
-  fontWeight: 'bold',
-  color: '#fff',
-  backgroundColor: '#2196f3',
-  border: 'none',
-  borderRadius: '4px',
-  cursor: 'pointer',
-  transition: 'background-color 0.3s',
-};
-
+// NEW
 const Questions = () => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [tryCounter, setTryCounter] = useState(0);
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+  const [shuffledAnswers, setShuffledAnswers] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [disabledOptions, setDisabledOptions] = useState([]);
+
   const { subjectID, classID, unitID, lessonID } = useParams();
 
   useEffect(() => {
@@ -56,8 +34,18 @@ const Questions = () => {
     fetchQuestions();
   }, [lessonID]);
 
+  useEffect(() => {
+    if (questions.length > 0) {
+      setShuffledAnswers(shuffleAnswers());
+    }
+  }, [questions, currentQuestionIndex]);
+
   const handleNextQuestion = () => {
     setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+    setTryCounter(0);
+    setIsAnswerCorrect(false);
+    setSelectedAnswer(null);
+    setDisabledOptions([]);
   };
 
   const currentQuestion = questions[currentQuestionIndex];
@@ -66,36 +54,106 @@ const Questions = () => {
     return <p>Loading...</p>;
   }
 
-  const handleAnswerSelect = (answer) => {
-    // Handle the selected answer here
-    console.log(answer);
+  const shuffleAnswers = () => {
+    const shuffledAnswers = [...currentQuestion.answers];
+    for (let i = shuffledAnswers.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledAnswers[i], shuffledAnswers[j]] = [shuffledAnswers[j], shuffledAnswers[i]];
+    }
+    return shuffledAnswers;
+  };
+
+  const handleAnswerSelect = (selectedAnswer) => {
+    const correctAnswer = currentQuestion.answers[0]; // Assuming the correct answer is always at the first index of the original array
+
+    if (selectedAnswer === correctAnswer) {
+      setIsAnswerCorrect(true);
+      setTimeout(() => {
+        handleNextQuestion();
+      }, 2000);
+    } else {
+      setTryCounter((prevCounter) => prevCounter + 1);
+      setDisabledOptions((prevDisabledOptions) => [...prevDisabledOptions, selectedAnswer]);
+    }
+    setSelectedAnswer(selectedAnswer);
+  };
+
+  const styles = {
+    // buttonContainer: {
+    //   display: 'flex',
+    //   flexDirection: 'column',
+    //   alignItems: 'flex-start',
+      
+    // },
+    button: {
+      padding: '10px',
+      margin: '5px',
+      backgroundColor: '#41D1C6',
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+      cursor: 'pointer',
+    },
+    correctButton: {
+      backgroundColor: 'green',
+      color: 'white',
+    },
+    incorrectButton: {
+      backgroundColor: 'grey',
+      color: 'white',
+    },
   };
 
   return (
     <div>
-      <Typography variant="h3" sx={{ mb: 0}}>
-      Quiz Page!
+      <Typography variant="h3" sx={{ mb: 0 }}>
+        Quiz Page!
       </Typography>
       <div>
-      <Typography variant="h3" sx={{ mb: 1 }}>
-      {currentQuestion.question_details}
-      </Typography>
+        <Typography variant="h3" sx={{ mb: 1 }}>
+          {currentQuestion.question_details}
+        </Typography>
         {currentQuestion.question_image && (
-           <img src={currentQuestion.question_image} alt={`Question ${currentQuestionIndex + 1}`} style={{ width: '300px', height: '300px' }}/>
+          <img
+            src={currentQuestion.question_image}
+            alt={`Question ${currentQuestionIndex + 1}`}
+            style={{ width: '300px', height: '300px', marginBottom: '20px'}}
+          />
         )}
-        {currentQuestion.answers.map((answer, index) => (
-          <button
-            key={index}
-            onClick={() => handleAnswerSelect(answer)}
-            style={buttonStyles}
-          >
-            {answer}
-          </button>
-        ))}
+        {/* <div style={ styles.buttonContainer }> */} 
+        {shuffledAnswers.map((answer, index) => {
+          const isCorrectAnswer = answer === currentQuestion.answers[0];
+          const isClickedIncorrectAnswer = selectedAnswer === answer && !isCorrectAnswer;
+          const isDisabled = disabledOptions.includes(answer);
+
+          let buttonStyle = styles.button;
+          if (isAnswerCorrect && isCorrectAnswer) {
+            buttonStyle = { ...buttonStyle, ...styles.correctButton };
+          } else if (isClickedIncorrectAnswer) {
+            buttonStyle = { ...buttonStyle, ...styles.incorrectButton };
+          }
+
+          return (
+            <button
+              key={index}
+              onClick={() => handleAnswerSelect(answer)}
+              style={isDisabled ? { ...buttonStyle, ...{ backgroundColor: 'grey', cursor: 'not-allowed' } } : buttonStyle}
+              disabled={isAnswerCorrect || isDisabled}
+            >
+              {answer}
+            </button>
+          );
+        })}
+        {/* </div> */}
+        {tryCounter > 0 && !isAnswerCorrect && (
+          <p>You have tried {tryCounter} time(s). Try again!</p>
+        )}
+        {isAnswerCorrect && (
+          <p>Correct! The answer is {currentQuestion.answers[0]}.</p>
+        )}
       </div>
-      <button onClick={handleNextQuestion} style={nextButtonStyles}>Next</button>
     </div>
   );
 };
 
 export default Questions;
+
